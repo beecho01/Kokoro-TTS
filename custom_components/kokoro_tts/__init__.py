91eb8e1e39b05fc15e78ed94f2a3522a426fe73f
James Beeching <98554230+beecho01@users.noreply.github.com>
2025-09-23 13:58:28 +0100

    Changes made to conform to Hassfest and Hacs validation actions

--- custom_components/kokoro_tts/__init__.py
+++ custom_components/kokoro_tts/__init__.py
@@ -3,10 +3,13 @@ from __future__ import annotations
 from homeassistant.const import Platform
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
 
 from .const import DOMAIN
 
 PLATFORMS = [Platform.TTS]
+
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     """Set up the Kokoro TTS component."""
